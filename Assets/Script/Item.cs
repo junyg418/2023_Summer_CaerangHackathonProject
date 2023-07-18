d6fd4d686987bff982b54d0c5fb04473c7cb66b4
sangim04 <128368686+sangim04@users.noreply.github.com>
2023-07-18 11:33:03 +0900

    인벤토리 1차

---
+++ Assets/Script/Item.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Item : ScriptableObject
+{
+    public string itemName;
+    public string price;
+    public Sprite itemImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
