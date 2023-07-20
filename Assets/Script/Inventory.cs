0b0d298ebd6bfb1f6570f192f387e2f8219977db
sangim04 <128368686+sangim04@users.noreply.github.com>
2023-07-20 14:59:08 +0900

    아이템 넣을 인벤토리 스크립트 추가
    아이템을 얻는 스크립트를 완성한다면
    아이템을 얻었을 때
    inven오브젝트의 Inspector창에 보면 Iventory스크리브를 컴포넌트 했거든? 거기 안에 Items리스트에 아이템을 넣게 만들어줭

--- Assets/Script/Inventory.cs
+++ Assets/Script/Inventory.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SlotBtn : MonoBehaviour
+public class Inventory : MonoBehaviour
 {
-    
+    [SerializeField]
+    private ItemData[] _items;
     // Start is called before the first frame update
     void Start()
     {
