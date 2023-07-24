f1b80e63edb0faa92789b8b4b51b29b142c17952
이준영 <fgh33678@gmail.com>
2023-07-24 16:36:01 +0900

    1차 복구 완료
    slot 동적 표시와 개수 표시제작
    + 아이템 얻는 메소드 제작

--- Assets/Script/Inventory.cs
+++ Assets/Script/Inventory.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Threading;
 using TMPro;
+using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,28 +13,26 @@ public class Inventory : MonoBehaviour
     //public ItemData[] _items;
     private SaveInventory saveInventory = new SaveInventory();
     private Dictionary<int, int> save_data = new Dictionary<int, int>();
+    private GameObject[] objectsSlot;
 
+    private void Awake()
+    {
+        objectsSlot = GameObject.FindGameObjectsWithTag("Inventory_slot"); // slot 배열
+        
+    }
     void Start()
     {
-        GameObject[] objectsSlot = GameObject.FindGameObjectsWithTag("Inventory_slot"); // slot 배열
         foreach (GameObject obj in objectsSlot)
         {
             obj.SetActive(false);
         }
 
         // 저장된 데이터가 있을 때
+        if(saveInventory.LoadData())
             save_data = saveInventory.inventory_data; // 딕셔너리 형태
-            save_data.Add(1, 3);
-            var save_data_keys = new List<int>(save_data.Keys); // 딕셔너리의 key 의 배열
-
-            int length = save_data.Count; // 딕셔너리의 길이
-        for (int idx = 0; idx < length; idx++)
-        {
-            GameObject current_slot = objectsSlot[idx];
-            current_slot.SetActive(true);
-            set_slot_count(current_slot, save_data_keys[idx]);
-        }
-
+        save_data.Add(1, 3);
+        save_data.Add(2, 6);
+        init_slot();
     }
 
     // Update is called once per frame
@@ -42,6 +41,51 @@ public class Inventory : MonoBehaviour
         
     }
     
+    public void get_item()
+    {
+        int randomInt = Random.Range(1, 101);
+
+        // 뼈다귀 50%
+        if (1 <= randomInt && randomInt < 50)
+            append_item_to_inventory(1);
+        else if (50 <= randomInt && randomInt < 80)
+            append_item_to_inventory(2);
+        else if (50 <= randomInt && randomInt < 101)
+            Debug.Log("3번 떴음");
+            //append_item_to_inventory(3);
+
+        init_slot();
+    }
+    /// <summary>
+    /// 인벤토리에 아이템 추가하는 작업
+    /// </summary>
+    /// <param name="item_id">아이템의 id 를 입력받음</param>
+    private void append_item_to_inventory(int item_id)
+    {
+        if (save_data.ContainsKey(item_id))
+        {
+            if (save_data[item_id] >= 99)
+                return;
+            else
+                save_data[item_id] += 1;
+        }
+        else
+            save_data[item_id] = 1;
+    }
+
+    private void init_slot()
+    {
+        var save_data_keys = new List<int>(save_data.Keys); // 딕셔너리의 key 의 배열
+
+        int length = save_data.Count; // 딕셔너리의 길이
+        for (int idx = 0; idx < length; idx++)
+        {
+            GameObject current_slot = objectsSlot[idx];
+            current_slot.SetActive(true);
+            set_slot_count(current_slot, save_data_keys[idx]);
+        }
+    }
+
     private void set_slot_count(GameObject current_slot, int item_id)
     {
         TextMeshProUGUI child_text_component = current_slot.transform.GetComponentInChildren<TextMeshProUGUI>();
@@ -49,36 +93,15 @@ public class Inventory : MonoBehaviour
     }
 }
 
-//public class SlotData : MonoBehaviour
-//{
-//    public ItemData item_data;
-//    private int item_count;
-//
-//    public GameObject parentObject;
-//    public GameObject uiTextPrefab;
-//
-//    public SlotData(ItemData item_data, int count)
-//    {
-//        this.item_data = item_data;
-//        this.item_count = count;
-//    }
-//    private void Start()
-//    {
-//        GameObject uiTextObject = Instantiate(uiTextPrefab, parentObject.transform);
-//
-//        UnityEngine.UI.Text uiTextComponent = uiTextObject.GetComponent<UnityEngine.UI.Text>();
-//        uiTextComponent.text = string.Format("+ {0}", item_count);
-//    }
-//}
 
 public class SaveInventory
 {
     public Dictionary<int, int> inventory_data = new Dictionary<int, int>();
     private string filePath;
 
-    public void SaveData()
+    public void SaveData(Dictionary<int, int> save_dict)
     {
-        string jsonData = JsonUtility.ToJson(inventory_data);
+        string jsonData = JsonUtility.ToJson(save_dict);
 
         filePath = Path.Combine(Application.persistentDataPath, "data.json");
         File.WriteAllText(filePath, jsonData);
