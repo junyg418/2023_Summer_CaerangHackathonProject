64c374f6763afcc47d3a88629372dbb71b1b7322
용상임 <sangim041113@gmail.com>
2023-07-24 16:16:46 +0900

    Update GetItemEvent.cs

--- Assets/Script/GetItemEvent.cs
+++ Assets/Script/GetItemEvent.cs
@@ -11,22 +11,28 @@ public class GetItemEvent : MonoBehaviour
     //public SpriteRenderer spriterenderer;
     public KeyCode interactionKey = KeyCode.Space;
 
-    public float time_to_get_item = 4.0f;
-    private float keyPressStartTime;
-    private bool isKeyHold = false;
-
     public Slider slider;
+    public Canvas canvas;
+
+    private Rigidbody2D rb;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         default_find_img.SetActive(false);
+        canvas.enabled = false;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (!Input.GetKey(KeyCode.Space))
+        {
+            rb.constraints = RigidbodyConstraints2D.None | RigidbodyConstraints2D.FreezeRotation;
+            slider.value = 0;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -36,6 +42,7 @@ public class GetItemEvent : MonoBehaviour
             return;
 
         default_find_img.SetActive(true);
+        canvas.enabled = true;
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
@@ -43,40 +50,37 @@ public class GetItemEvent : MonoBehaviour
         if (!target.tag.Equals("GoPoint"))
             return;
 
-        if (Input.GetKeyDown(interactionKey))
+        if (Input.GetKey(interactionKey))
         {
-            isKeyHold = true;
-            keyPressStartTime = Time.time;
             StartCoroutine(ActiveObjcetForSecond(target));
         }
-        else if (Input.GetKeyUp(interactionKey))
-        {
-            isKeyHold = false;
-        }
+    
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
         GameObject target = collision.gameObject;
         if (!target.tag.Equals("GoPoint"))
             return;
+        slider.value = 0;
 
         default_find_img.SetActive(false);
+        canvas.enabled = false;
     }
 
     IEnumerator ActiveObjcetForSecond(GameObject target)
     {
-        while (isKeyHold)
+        while (Input.GetKey(KeyCode.Space))
         {
-            float passed_time = Time.time - keyPressStartTime;
-            
+            rb.constraints = RigidbodyConstraints2D.FreezePosition | RigidbodyConstraints2D.FreezeRotation;
 
-            if (passed_time >= time_to_get_item)
+            slider.value += 0.001f;
+
+            if (slider.value == 1)
             {
                 Debug.Log("아이템 얻음!");
                 Destroy(target);
                 yield break;
             }
-
                 yield return new WaitForSeconds(0.1f);
         }
     }
