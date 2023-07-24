52756cbbfad8d203306498dce756fcc14b48339d
용상임 <sangim041113@gmail.com>
2023-07-24 14:17:53 +0900

    1차

--- Assets/Script/GetItemEvent.cs
+++ Assets/Script/GetItemEvent.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 using static UnityEngine.GraphicsBuffer;
 
 public class GetItemEvent : MonoBehaviour
@@ -9,10 +10,12 @@ public class GetItemEvent : MonoBehaviour
     public GameObject default_find_img;
     //public SpriteRenderer spriterenderer;
     public KeyCode interactionKey = KeyCode.Space;
-    
-    public float time_to_get_item = 3.0f;
+
+    public float time_to_get_item = 4.0f;
     private float keyPressStartTime;
-    private bool isKeyHold = false; 
+    private bool isKeyHold = false;
+
+    public Slider slider;
 
     // Start is called before the first frame update
     void Start()
@@ -29,7 +32,7 @@ public class GetItemEvent : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         GameObject target = collision.gameObject;
-        if (!target.tag.Equals("GoPoint")) 
+        if (!target.tag.Equals("GoPoint"))
             return;
 
         default_find_img.SetActive(true);
@@ -37,22 +40,24 @@ public class GetItemEvent : MonoBehaviour
     private void OnTriggerStay2D(Collider2D collision)
     {
         GameObject target = collision.gameObject;
-        if (!target.tag.Equals("GoPoint")) 
+        if (!target.tag.Equals("GoPoint"))
             return;
 
-        if (Input.GetKeyDown(interactionKey)){
+        if (Input.GetKeyDown(interactionKey))
+        {
             isKeyHold = true;
             keyPressStartTime = Time.time;
             StartCoroutine(ActiveObjcetForSecond(target));
         }
-        else if (Input.GetKeyUp(interactionKey)){
+        else if (Input.GetKeyUp(interactionKey))
+        {
             isKeyHold = false;
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
         GameObject target = collision.gameObject;
-        if (!target.tag.Equals("GoPoint")) 
+        if (!target.tag.Equals("GoPoint"))
             return;
 
         default_find_img.SetActive(false);
@@ -63,21 +68,17 @@ public class GetItemEvent : MonoBehaviour
         while (isKeyHold)
         {
             float passed_time = Time.time - keyPressStartTime;
+
             if (passed_time >= time_to_get_item)
             {
                 Debug.Log("아이템 얻음!");
                 Destroy(target);
                 yield break;
             }
-            else if(passed_time >= 2.0f)
-            {
-                Debug.Log("2초 지남");
-            }
-            else if(passed_time >= 1.0f)
-            {
-                Debug.Log("1초 지남");
-            }
+           
             yield return new WaitForSeconds(0.1f);
         }
     }
 }
+    
+
