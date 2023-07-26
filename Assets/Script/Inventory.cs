6bee8e8562b997c45b27efad848982dee5ff4bcd
이준영 <fgh33678@gmail.com>
2023-07-26 13:40:04 +0900

    information set 함수 제작 완 및 이미지 추가 함수 제작 진행중

--- Assets/Script/Inventory.cs
+++ Assets/Script/Inventory.cs
@@ -6,6 +6,7 @@ using TMPro;
 using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEditor.Progress;
 
 public class Inventory : MonoBehaviour
 {
@@ -20,11 +21,11 @@ public class Inventory : MonoBehaviour
 
     private void Awake()
     {
-        objectsSlot = GameObject.FindGameObjectsWithTag("Inventory_slot"); // slot 배열
-        
+        objectsSlot = GameObject.FindGameObjectsWithTag("Inventory_slot"); // slot 배열mw
     }
     void Start()
     {
+        // slot init
         foreach (GameObject obj in objectsSlot)
         {
             obj.SetActive(false);
@@ -36,12 +37,15 @@ public class Inventory : MonoBehaviour
         save_data.Add(1, 3);
         save_data.Add(2, 6);
         init_slot();
+
+        // information init
+        set_information();
     }
 
     // Update is called once per frame
     void Update()
-    {
-       
+    {  
+
     }
     
     public void get_item()
@@ -50,9 +54,10 @@ public class Inventory : MonoBehaviour
 
         // 뼈다귀 50%
         if (1 <= randomInt && randomInt < 50)
-            append_item_to_inventory(1);
+            append_item_to_inventoryData(1);
+        // 버섯 30%
         else if (50 <= randomInt && randomInt < 80)
-            append_item_to_inventory(2);
+            append_item_to_inventoryData(2);
         else if (50 <= randomInt && randomInt < 101)
             Debug.Log("3번 떴음");
             //append_item_to_inventory(3);
@@ -63,7 +68,7 @@ public class Inventory : MonoBehaviour
     /// 인벤토리에 아이템 추가하는 작업
     /// </summary>
     /// <param name="item_id">아이템의 id 를 입력받음</param>
-    private void append_item_to_inventory(int item_id)
+    private void append_item_to_inventoryData(int item_id)
     {
         if (save_data.ContainsKey(item_id))
         {
@@ -75,7 +80,9 @@ public class Inventory : MonoBehaviour
         else
             save_data[item_id] = 1;
     }
-
+    /// <summary>
+    /// slot 초기화하는 function
+    /// </summary>
     private void init_slot()
     {
         var save_data_keys = new List<int>(save_data.Keys); // 딕셔너리의 key 의 배열
@@ -88,11 +95,43 @@ public class Inventory : MonoBehaviour
             set_slot_count(current_slot, save_data_keys[idx]);
         }
     }
-
+    /// <summary>
+    /// slot의 아이템 개수를 초기화해주는 function
+    /// </summary>
+    /// <param name="current_slot">현재 slot</param>
+    /// <param name="item_id">아이템의 id</param>
     private void set_slot_count(GameObject current_slot, int item_id)
     {
         TextMeshProUGUI child_text_component = current_slot.transform.GetComponentInChildren<TextMeshProUGUI>();
         child_text_component.text = string.Format("X {0}", save_data[item_id]);
+    }
+    private void set_slot_img(GameObject current_slot, int item_id)
+    {
+
+    }
+    // information 관련 function
+    private void set_information()
+    {
+        set_information_text("");
+        set_information_toolTip("");
+    }
+    private void set_information(ItemData item)
+    {
+        set_information_text(item.itemName);
+        set_information_toolTip(item.Tooltip);
+    }
+    /// information function
+    private void set_information_text(string input_text="")
+    {
+        GameObject information_text_gameObject = information_object.transform.GetChild(0).gameObject;
+
+        information_text_gameObject.GetComponent<Text>().text = input_text;
+    }
+    private void set_information_toolTip(string input_text="")
+    {
+        GameObject information_toolTip_gameObject = information_object.transform.GetChild(1).gameObject;
+
+        information_toolTip_gameObject.GetComponent<Text>().text = input_text;
     }
 }
 
