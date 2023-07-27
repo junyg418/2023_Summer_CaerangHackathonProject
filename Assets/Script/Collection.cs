75d6f6c01549970ae72771b8dc04acb078ad4ded
용상임 <sangim041113@gmail.com>
2023-07-27 16:03:34 +0900

    버그 고치려고함

--- Assets/Script/Collection.cs
+++ Assets/Script/Collection.cs
@@ -10,7 +10,7 @@ public class Collection : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        image = image.GetComponent<Image>();
+        image = image.GetComponent<Image>() ;
     }
 
     // Update is called once per frame
