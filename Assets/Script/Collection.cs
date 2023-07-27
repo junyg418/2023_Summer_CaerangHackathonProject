5cd9af5f8b6a714e7aa2e7215c9228eb1f24674e
용상임 <sangim041113@gmail.com>
2023-07-27 13:59:59 +0900

    아이템 검정 흰색

--- Assets/Script/Collection.cs
+++ Assets/Script/Collection.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Collection : MonoBehaviour
 {
+    public GameObject slot;
+    public Image image;
     // Start is called before the first frame update
     void Start()
     {
-        
+        image = slot.GetComponent<Image>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.T))
+            image.color = new Color(1, 1, 1);
     }
 }
