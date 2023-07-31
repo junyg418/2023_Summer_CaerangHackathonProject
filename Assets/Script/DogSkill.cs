198a64d61773ba2de1bc072057f3738efa485fab
이준영 <fgh33678@gmail.com>
2023-07-31 18:01:58 +0900

    갱아지 이동, 논리 구현 완료 but 아이템 안먹어짐

--- Assets/Script/DogSkill.cs
+++ Assets/Script/DogSkill.cs
@@ -6,17 +6,24 @@ public class DogSkill : MonoBehaviour
 {
     List<Vector3> sc_arr;
     Vector3 goal_pos;
+    public float move_speed = 3.0f;
+
     void Start()
     {
-        
     }
 
     void Update()
     {
-        if(RandomItemPoint.point_pos_array != null)
-        {
+        if (RandomItemPoint.point_pos_array != null)
             sc_arr = RandomItemPoint.point_pos_array;
+            goal_pos = sc_arr[0];
+
+        if (sc_arr[0] != goal_pos)
+        {
+            goal_pos = sc_arr[0];
         }
 
+        Vector3 direction = goal_pos - transform.position;
+        transform.position = Vector3.MoveTowards(transform.position, goal_pos, move_speed*Time.deltaTime);
     }
 }
