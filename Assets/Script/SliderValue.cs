8d9e55af61304df877962c6235bb012219d58a49
용상임 <sangim041113@gmail.com>
2023-07-31 14:18:52 +0900

    슬라이더 완료
    이제 개수 판매 만들기해야함

---
+++ Assets/Script/SliderValue.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderValue : MonoBehaviour
+{
+
+    Text valueText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        valueText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    public void valueUpdate(float value)
+    {
+        valueText.text = Mathf.RoundToInt(value)+"°³";
+    }
+}
