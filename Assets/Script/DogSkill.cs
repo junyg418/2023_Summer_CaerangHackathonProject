f9c978fc922c1cc72aba18e0a30d0e33c61acaeb
이준영 <fgh33678@gmail.com>
2023-08-01 11:17:56 +0900

    강아지 기다려 완성

--- Assets/Script/DogSkill.cs
+++ Assets/Script/DogSkill.cs
@@ -4,26 +4,49 @@ using UnityEngine;
 
 public class DogSkill : MonoBehaviour
 {
-    List<Vector3> sc_arr;
-    Vector3 goal_pos;
-    public float move_speed = 3.0f;
+    [SerializeField]
+    public float move_speed = 1.5f;
+    public float pauseTime = 30f;
 
-    void Start()
-    {
-    }
+
+    private List<Vector3> sc_arr;
+    private Vector3 goal_pos;
+    private bool updatePaused = false;
+    private float passedTime = 0f;
+    private bool first_flg = true;
 
     void Update()
     {
         if (RandomItemPoint.point_pos_array != null)
+        {
             sc_arr = RandomItemPoint.point_pos_array;
-            goal_pos = sc_arr[0];
+            if(first_flg)
+            {
+                goal_pos = sc_arr[0];
+                first_flg = false;
+            }
+        }
+
+        if (updatePaused)
+        {
+            passedTime += Time.deltaTime;
+            if (passedTime > pauseTime)
+            {
+                passedTime = 0f;
+                updatePaused = false;
+
+            }
+            return;
+        }
+
 
         if (sc_arr[0] != goal_pos)
         {
             goal_pos = sc_arr[0];
+            updatePaused = true;
+            Debug.Log("∏ÿ√„ Ω√¿€?");
         }
-
         Vector3 direction = goal_pos - transform.position;
-        transform.position = Vector3.MoveTowards(transform.position, goal_pos, move_speed*Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, goal_pos, move_speed * Time.deltaTime);
     }
 }
