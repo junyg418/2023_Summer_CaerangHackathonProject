5a1aebd4687f5d3802f64bf6cc21cb5ae3398481
용상임 <sangim041113@gmail.com>
2023-08-02 02:45:11 +0900

    상점 비활성화 인벤 오류 해결
    상점 비활성화 후 실행하면 아이템이 인벤토리에 들어가지 않던 오류 해결

--- Assets/Script/Menu.cs
+++ Assets/Script/Menu.cs
@@ -6,6 +6,12 @@ public class Menu : MonoBehaviour
 {
     void Start()
     {
+        menu.SetActive(true);
+        Invoke("DeactivateMenu", 0.1f);
+    }
+    private void DeactivateMenu()
+    {
+        // 메뉴를 비활성화합니다.
         menu.SetActive(false);
     }
 
