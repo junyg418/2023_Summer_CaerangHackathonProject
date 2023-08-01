7e6272adcee4c2cf2e707432db66cdad043eef9b
이준영 <fgh33678@gmail.com>
2023-08-01 14:56:32 +0900

    fin wifi 알고리즘

--- Assets/Script/MetalDetactSkill.cs
+++ Assets/Script/MetalDetactSkill.cs
@@ -15,23 +15,35 @@ public class MetalDetactSkill : MonoBehaviour
     public Sprite sec_wifi;
     public Sprite max_wifi;
 
-    public GameObject gameObject;
-
     private Sprite sprite_obj;
+    private Vector3 goal_vector;
+    private float min_dis;
 
     void Start()
     {
+        min_dis = float.MaxValue;
         sprite_obj = GetComponent<SpriteRenderer>().sprite;
     }
 
     void Update()
     {
-        
+        min_dis = float.MaxValue;
+        foreach(Vector3 data in RandomItemPoint.point_pos_array)
+        {
+            float tmp_dis = (data - transform.position).sqrMagnitude;
+            if(tmp_dis < min_dis)
+            {
+                min_dis = tmp_dis;
+                goal_vector = data;
+            }
+        }
+        set_wifi_img(goal_vector);
     }
 
     private void set_wifi_img(Vector3 goal_pos)
     {
         float distance = Vector3.Distance(transform.position, goal_pos);
+        Debug.Log(goal_pos + " " + distance);
         if (distance < max_dis)
         {
             sprite_obj = max_wifi;
