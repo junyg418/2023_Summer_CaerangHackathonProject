162d4b80dc72d63b5debb474598135d40565bf89
이준영 <fgh33678@gmail.com>
2023-08-01 13:02:38 +0900

    wifi img 추가

--- Assets/Script/Menu.cs
+++ Assets/Script/Menu.cs
@@ -5,18 +5,6 @@ using UnityEngine;
 public class Menu : MonoBehaviour
 {
     public GameObject menu;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void Openmenu()
     {
         if (menu.activeSelf == false)
